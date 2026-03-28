8e881a0dae63ba2fb5e1fe148f5c4f5770a16346
vibhu gupta <vibhugupta022@gmail.com>
2026-03-28 13:51:48 +0530

    Fix SPA routing for Render deployment

--- Server/controller/ChinemaHallController/ReadCinema_location.js
+++ Server/controller/ChinemaHallController/ReadCinema_location.js
@@ -1,8 +1,17 @@
+import { redis } from "../../App.js";
 import Cinema from "../../models/CinemaHallSchema.js";
 
 export async function ReadcinemaWithLocation(req, res) {
   try {
     const { StateId, CityId } = req.query;
+
+    const cacheExists = await redis.exists(`cinema_${StateId}_${CityId}`);
+    if (cacheExists) {
+      console.log("redis hit");
+      const cinemas = await redis.get(`cinema_${StateId}_${CityId}`);
+      console.log(cinemas);
+      return res.status(200).json(JSON.parse(cinemas));
+    }
 
     if (!StateId || !CityId) {
       return res.status(400).json({ message: "both state and city needed" });
@@ -13,7 +22,7 @@ export async function ReadcinemaWithLocation(req, res) {
     if (cinema.length === 0) {
       return res.status(404).json({ message: "no cinema found" });
     }
-
+   await redis.setex(`cinema_${StateId}_${CityId}` , 300 , JSON.stringify(cinema))
     return res.status(200).json(cinema);
   } catch (err) {
     return res.status(500).json({ message: "error in ReadcinemaWithLocation" });
